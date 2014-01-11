125e1c80cd87a9a37b3c7a4c7f5cc43bf62e11a7
pcputta <prabodhcp@gmail.com>
2014-01-11 11:59:49 +0530

    Changes to table structure

--- VTU/Sem5/DB_Lab/Question3/3i_create.sql
+++ VTU/Sem5/DB_Lab/Question3/3i_create.sql
@@ -2,36 +2,45 @@ CREATE DATABASE COURSEADOPTION;
 USE COURSEADOPTION;
 
 CREATE TABLE IF NOT EXISTS STUDENT 
-  ( regno  VARCHAR(9) PRIMARY KEY,
+  ( regno  VARCHAR(9),
     sname  VARCHAR(15),
     major  VARCHAR(20),
-    bdate  DATE
+    bdate  DATE,
+	PRIMARY KEY(regno)
   );
 
 CREATE TABLE IF NOT EXISTS COURSE
-  ( courseno  INTEGER(9) PRIMARY KEY,
+  ( courseno  INTEGER(9),
     cname  VARCHAR(20),
-    dept   VARCHAR(20)
+    dept   VARCHAR(20),
+	PRIMARY KEY(courseno)
   );
 
 CREATE TABLE IF NOT EXISTS ENROL
-  ( regno  VARCHAR(9) REFERENCES STUDENT(regno),
-    courseno INTEGER(9) REFERENCES COURSE(courseno),
+  ( regno  VARCHAR(9),
+    courseno INTEGER(9),
     sem    INTEGER(9) ,
-    marks  INTEGER(5)
+    marks  INTEGER(5),
+	PRIMARY KEY(regno,courseno),
+	FOREIGN KEY(regno) REFERENCES STUDENT(regno),
+	FOREIGN KEY(courseno) REFERENCES COURSE(courseno)
   );
 
 CREATE TABLE IF NOT EXISTS TEXT
-  ( book_isbn  INTEGER(9) PRIMARY KEY,
+  ( book_isbn  INTEGER(9),
     book_title  VARCHAR(30),
     publisher  VARCHAR(23),
-    author   VARCHAR(27)
+    author   VARCHAR(27),
+	PRIMARY KEY(book_isbn)
   );
 
 CREATE TABLE IF NOT EXISTS BOOK_ADOPTION
   ( courseno  INTEGER(9) REFERENCES COURSE(courseno),
-    sem   INTEGER(9),
-    book_isbn  INTEGER(9) REFERENCES TEXT(book_isbn) 
+    book_isbn  INTEGER(9) REFERENCES TEXT(book_isbn),
+	sem   INTEGER(9),
+	PRIMARY KEY(courseno,book_isbn),
+	FOREIGN KEY(courseno) REFERENCES COURSE(courseno),
+	FOREIGN KEY(book_isbn) REFERENCES TEXT(book_isbn)
   );
 
 
