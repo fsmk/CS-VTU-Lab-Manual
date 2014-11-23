de360bba85c454261160a18837a21a04c7240278
Abhijith M <m.abhijith04@gmail.com>
2014-11-23 16:17:58 +0530

    Update books_controller.rb

--- VTU/Sem7/WP_Lab/12_RailsApplication/books_controller.rb
+++ VTU/Sem7/WP_Lab/12_RailsApplication/books_controller.rb
@@ -1,10 +1,6 @@
-class BooksController < ApplicationController
-  def index
-  end
-  def add
-	Book.create(:access_no=> params[:b_access_no],:title=> params[:b_title],:author=> params[:b_author],:edition=> params[:b_edition],:publisher=> params[:b_publisher])
-	redirect_to :action => 'index'
-  end
-  def search
-  end
+class BooksController < ApplicationController      
+      def add
+            Book.create(:access_no=> params[:b_access_no],:title=> params[:b_title],:author=> params[:b_author],:edition=> params[:b_edition],:publisher=> params[:b_publisher])
+            redirect_to :action => 'index'
+      end      
 end
