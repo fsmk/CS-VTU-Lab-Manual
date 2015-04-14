8edb76085acbb06cf047a9f39152c02d2fa75e34
Arun Isaac <theroarofthedragon@gmail.com>
2015-04-15 01:32:52 +0530

    Experiment 5 - environment variables code modified and output added

--- VTU/Sem6/USP_CD_Lab/05aEnvironmentVariables/environment_variables.c
+++ VTU/Sem6/USP_CD_Lab/05aEnvironmentVariables/environment_variables.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
 
 int main (int argc, char *argv[])
 {
-	int i;
 	char **ptr;
 	extern char **environ;
-	for (ptr=environ; *ptr!=0;ptr++)
-	printf("%s\n",*ptr);
-	exit(0);
+	for (ptr = environ; *ptr != 0; ptr++)
+		printf("%s\n", *ptr);
+	return 0;
 }
