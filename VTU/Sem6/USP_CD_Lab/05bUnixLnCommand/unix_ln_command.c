6435ed607d97609805cdc9f8c15bff132beeb753
Arun Isaac <theroarofthedragon@gmail.com>
2015-05-27 19:04:43 +0530

    V/6/USP/05bUnixLnCommand: Improve code, add line by line code explanation
    Code improvements are
    * Improve indentation, spacing
    * Change incorrect argc return value to 1
    * Improve usage summary, and "link error" messages

--- VTU/Sem6/USP_CD_Lab/05bUnixLnCommand/unix_ln_command.c
+++ VTU/Sem6/USP_CD_Lab/05bUnixLnCommand/unix_ln_command.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <unistd.h>
+
 int main (int argc, char *argv[])
 {
 	if(argc != 3) {
-		printf("Usage: %s <src_file><dest_file>\n", argv[0]);
-		return 0;
+		printf("Usage: %s SRC DEST\n", argv[0]);
+		return 1;
 	}
-	if(link(argv[1],argv[2]) == -1) {
-		printf("Link Error\n");
-	return 1;
+	if(link(argv[1], argv[2]) == -1) {
+		printf("Link error\n");
+		return 1;
 	}
 	return 0;
 }
