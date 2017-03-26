c72c63ad0bd08ca376af83ec3f1e181f63555c25
Subhrajyoti Sen <subhrajyoti12@gmail.com>
2017-03-26 17:38:18 +0530

    V/6/USP/05aEnvironList: remove unneeded code

--- VTU/Sem6/USP_CD_Lab/05aEnvironList/5a_environlist.c
+++ VTU/Sem6/USP_CD_Lab/05aEnvironList/5a_environlist.c
@@ -1,12 +1,10 @@
 #include<stdio.h>
-#include<stdlib.h>
 #include<unistd.h>
 int main(int argc,char *argv[])
 {
-int i;
 char **ptr;
 extern char **environ;
 for(ptr=environ; *ptr!=0;ptr++)
 printf("%s\n",*ptr);
-exit(0);
+return 0;
 }
